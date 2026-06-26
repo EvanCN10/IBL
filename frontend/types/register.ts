c3fab6644d9ca42488ad3253cf53bdd964741ba8
NITSUJ <justinvalentino88@gmail.com>
2026-06-26 14:33:07 +0700

    chore(register): define TypeScript types and form constants

--- frontend/types/register.ts
+++ frontend/types/register.ts
@@ -1,7 +1,28 @@
 // TypeScript types & interfaces untuk form pendaftaran IBL
 
+export interface StepData {
+  shortAnswer1: string;
+  shortAnswer2: string;
+  shortAnswer3: string;
+  longAnswer1: string;
+  longAnswer2: string;
+  dropdownVal: string;
+  radioVal: string;
+}
+
+export interface UploadData {
+  cv: string;
+  ktm: string;
+  twibbon: string;
+  buktiFollow: string;
+  portofolio: string;
+}
+
 export interface RegisterFormData {
-  // TODO: tambahkan field-field form pendaftaran
+  informasiUmum: StepData;
+  subdivisi1: StepData;
+  subdivisi2: StepData;
+  uploadBerkas: UploadData;
 }
 
 export interface CountdownTime {
