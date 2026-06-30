c909b5b5eb94b145b8d518f217ef0eddf8adf2d4
NITSUJ <justinvalentino88@gmail.com>
2026-06-30 21:20:10 +0700

    fix(frontend): correct invalid z-index class for Tailwind

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Navbar = () => {
   return (
-    <header className="w-full h-[91px] bg-bone/75 z-100 mx-auto flex items-center gap-[12px] p-10 fixed">
+    <header className="w-full h-[91px] bg-bone/75 z-[100] mx-auto flex items-center gap-[12px] p-10 fixed">
       <Image alt="Logo IBL 2K26" src="/images/LOGO_1.svg" width={72.6} height={76} />
       <span className="font-hollywood text-[24px]">IBL 2K26</span>
     </header>
